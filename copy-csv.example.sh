ffb860c3fcf9486efe21483d38b6d7af4f9a7e72
mliang2 <changeme@bla.com>
2023-09-09 22:34:23 -0700

    disable scp password prompt; play error notification sound on scp error

--- copy-csv.example.sh
+++ copy-csv.example.sh
@@ -1,22 +1,22 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-scp_opts="-i ~/id_rsa.c2-temperature -P 22 -o ConnectTimeout=30 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=error"
+scp_opts="-i ~/id_rsa.c2-temperature -P 22 -o PasswordAuthentication=no -o ConnectTimeout=30 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=error"
 scp_target="user@IP:/tmp/"
 
 csv=~/leeco-temp.csv
-notification_sound="/system/media/audio/notifications/Iridium.ogg"
+ok_sound="/system/media/audio/notifications/Iridium.ogg"
+error_sound="/system/media/audio/notifications/Betelgeuse.ogg"
 
 if [ -f $csv ]; then
-    scp $scp_opts $csv $scp_target &> /dev/null
+    scp $scp_opts $csv $scp_target &> /dev/null; scp_rc=$?
 
-    # play a sound when csv is uploaded
-    # install w/ "apt install play-audio".  NEOS already have termux installed
-    if [ -f $notification_sound ]; then
-        play-audio $notification_sound
+    # play a sound when csv is uploaded. See README.md for instructions
+    if [ $scp_rc -eq 0 ]; then
+        play-audio $ok_sound
     else
-        # fallback
-        play-audio /system/media/audio/ui/camera_focus.ogg
+        play-audio $error_sound
     fi
 else
     echo "leeco-temp.csv not found"
+    play-audio $error_sound
 fi
